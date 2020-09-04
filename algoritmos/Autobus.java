ad5c383061d219e9e0752204c73d2f1e0c0019b4
Alisson1105 <70778534+Alisson1105@users.noreply.github.com>
2020-09-04 17:26:23 -0400

    Add files via upload

---
+++ algoritmos/Autobus.java
@@ -0,0 +1,13 @@
+
+public class Autobus
+{
+
+    public double Autobus()
+    {
+        int pase=45;
+        double valorporviaje=1.80;
+        double res= pase/valorporviaje;
+        return res;
+    }
+
+}
---
+++ algoritmos/Copiadeseguridad.java
@@ -0,0 +1,17 @@
+
+
+public class Copiadeseguridad
+{
+    
+    public int Copiadeseguridad()
+    {
+        int informacion=5;
+        int cd=700;
+        int valor=1024;
+        int equivalencia= valor*informacion;
+        int res= equivalencia/cd;
+        return res;
+        
+    }
+
+}
